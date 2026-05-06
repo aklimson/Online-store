dcfd09d79058a57eedafbf0242dc2e2bff94cd4c
LungoweA <lungowe3a@gmail.com>
2026-05-06 22:14:01 +0200

    feat:added navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
-  return null;
+  return (
+    <header className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">ElectroStore</Link>
+      </div>
+
+      <nav className="navbar-links">
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/about">About</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign up</Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
