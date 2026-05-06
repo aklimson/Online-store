fc4698eb029c1b0720b95ec8a2b0a3335887686e
Anastasia Klimson <anastasia.klimson0051@stud.hkr.se>
2026-05-06 23:34:23 +0200

    Add product descriptions and buttons

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -6,21 +6,25 @@ function Products() {
       id: 1,
       name: "Product Name 1",
       price: "999 SEK",
+      description: "Short product description will be shown here.",
     },
     {
       id: 2,
       name: "Product Name 2",
       price: "1299 SEK",
+      description: "Short product description will be shown here.",
     },
     {
       id: 3,
       name: "Product Name 3",
       price: "799 SEK",
+      description: "Short product description will be shown here.",
     },
     {
       id: 4,
       name: "Product Name 4",
       price: "1499 SEK",
+      description: "Short product description will be shown here.",
     },
   ];
 
@@ -35,7 +39,10 @@ function Products() {
         {products.map((product) => (
           <div className="product-item" key={product.id}>
             <h2>{product.name}</h2>
-            <p>{product.price}</p>
+            <p className="product-description">{product.description}</p>
+            <p className="product-price">{product.price}</p>
+
+            <button className="product-button">View product</button>
           </div>
         ))}
       </section>
