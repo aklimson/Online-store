0bcc68aec73278c3adabae428aa24bd79aed1462
Michael2024-coder <Agunbiademichael2017@gmail.com>
2026-05-13 12:57:09 +0200

    Modify JWT authentication and middleware protection

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -2,19 +2,17 @@ import User from '../models/schema/customerSchema.js';
 import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken';
 
-export const registerUser = async (req, res,next) => {
+export const registerUser = async (req, res, next) => {
     try {
         const { firstname, lastname, email, password, phoneNumber, address } = req.body;
 
-        const emailExist = await User.findOne({email});
-        if(emailExist) return res.status(400).json({msg: "Email already exists!"})
+        const emailExist = await User.findOne({ email });
+        if (emailExist) return res.status(400).json({ msg: "User already exists!" })
 
         const hashed = await bcrypt.hash(password, 10)
+
         const user = await User.create({ firstname, lastname, email, password: hashed, phoneNumber, address })
-        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
-            expiresIn: "1m"
-        });
-        return res.json({ token });
+        res.status(201).json({ message: "User registered successfully" });
 
     } catch (error) {
         next(error);
@@ -22,15 +20,12 @@ export const registerUser = async (req, res,next) => {
 
 }
 
-
-
-
-export const loginUser = async (req, res,next) => {
+export const loginUser = async (req, res, next) => {
     try {
         const { email, password } = req.body;
 
         const user = await User.findOne({ email });
-        if (!user) return res.status(400).json({ msg: 'Invalid credentials' });
+        if (!user) return res.status(400).json({ msg: 'Invalid email or password' });
 
         const match = await bcrypt.compare(password, user.password)
         if (!match) return res.status(400).json({ msg: 'Invalid email or password' });
@@ -38,7 +33,15 @@ export const loginUser = async (req, res,next) => {
         const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
             expiresIn: "1m"
         });
-        return res.json({ token });
+        return res.status(200).json({
+            token,
+            user: {
+                id: user._id,
+                firstname: user.firstname,
+                email: user.email,
+                role: user.role
+            }
+        });
 
 
     } catch (error) {
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import productRoute from "./productRoute.js";
+import authRoute from "./authRoute.js"
+import { authMiddleware } from '../middleware/authMiddleware.js';
+import { adminMiddleware } from "../middleware/adminMiddleware.js";
 
 export const router = express.Router();
 
 router.use("/api", productRoute);
+router.use("/api,",)
